ba27f86bcc549dbdbd88c511d5128cbec3a29367
Truitt Thuecks <leslie@Truitts-Macbook.local>
2023-10-09 14:44:08 -0400

    Initial Commit

---
+++ Seasons.cs/Seasons.cs
@@ -0,0 +1,34 @@
+﻿namespace Seasons.cs;
+
+class Seasons
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter the name of a season and I'll translate it to Spanish\n");
+        string seasonChoice = Console.ReadLine();
+
+        switch (seasonChoice.ToLower())
+        {
+            case "spring":
+                Console.WriteLine("Spring in Spanish is 'la primavera'.");
+                break;
+
+            case "summer":
+                Console.WriteLine("Summer in Spanish is 'el verano'.");
+                break;
+
+            case "fall":
+            case "autumn":
+                Console.WriteLine("Fall and Autumn in Spanish is 'el otoño'.");
+                break;
+
+            case "winter":
+                Console.WriteLine("Winter in Spanish is 'el invierno'.");
+                break;
+
+        }
+
+    }
+}
+
